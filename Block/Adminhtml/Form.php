d50f8b782ec51397df2a267f90a15151b57c71db
Iman Aboheydary <iranimij@gmail.com>
2024-11-18 21:53:38 +0100

    Add admin form

---
+++ Block/Adminhtml/Form.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Iranimij\ProductLabels\Block\Adminhtml;
+
+use Magento\Backend\Block\Widget\Form\Generic;
+
+class Form extends Generic
+{
+}
---
+++ Controller/Adminhtml/New/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Iranimij\ProductLabels\Controller\Adminhtml\New;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    public function __construct(
+        Context $context,
+        private readonly PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
